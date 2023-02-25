3a2d28637b51037b3b3a3edaf57553dcb08e2475
Karthikeyan Kumaran <karthikeyan.asus@outlook.com>
2023-02-25 13:21:11 +0530

    [Leetcode] Added reference folder for STL library basic APIs
    a. Description
       - added unordered_set.cpp in reference folder
    
    Signed-off-by: Karthikeyan Kumaran <karthikeyan.asus@outlook.com>

---
+++ reference/unordered_set.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+
+
+
+bool containsDuplicate(vector<int>& nums) {
+        
+    unordered_set<int> simplehash;
+    unordered_set<int> :: iterator itr;
+    int len = nums.size();
+    
+    for(int i = 0; i < len; i++)
+    {
+        // get the itr as result of finding the entry
+        itr = simplehash.find(nums[i]);
+        // if the entry is already present
+        if(itr != simplehash.end())
+        {
+               return true;
+        }
+        
+        // add the entry to the set
+        simplehash.insert(nums[i]);
+    }
+    return false;
+}
+
+
+int main()
+{
+    vector<int> nums;
+    nums.push_back(1);
+
+    if(containsDuplicate(nums))
+        cout << "duplicate" << endl;
+    else
+        cout << "unique" << endl;    
+}
+
+// @lc code=end
+
